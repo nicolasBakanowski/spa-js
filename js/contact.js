b3d63d0e4dac848fcc75af6e7dd8750145c4feb5
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2024-05-14 09:06:37 -0300

    add simple form

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var turnoLink = document.getElementById('turno-link');
+    let turnoLink = document.getElementById('turno-link');
 
     turnoLink.addEventListener('click', function(event) {
         event.preventDefault();
 
-        alert('Abre un formulario de contacto para sacar un turno');
+        let form = document.createElement('form');
+
+        let nombreInput = document.createElement('input');
+        nombreInput.setAttribute('type', 'text');
+        nombreInput.setAttribute('placeholder', 'Nombre');
+        nombreInput.setAttribute('name', 'nombre');
+        form.appendChild(nombreInput);
+
+        let horaInput = document.createElement('input');
+        horaInput.setAttribute('type', 'time');
+        horaInput.setAttribute('name', 'hora');
+        form.appendChild(horaInput);
+
+        let submitButton = document.createElement('button');
+        submitButton.setAttribute('type', 'submit');
+        submitButton.textContent = 'Enviar';
+        form.appendChild(submitButton);
+
+        let contactoSection = document.getElementById('contacto');
+        contactoSection.appendChild(form);
+
+        turnoLink.parentNode.removeChild(turnoLink);
     });
 });
